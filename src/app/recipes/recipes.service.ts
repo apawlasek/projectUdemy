67bdcdf118b4f103d2be266ac194302dc7d3b735
Anna Pawlasek <anna.pawlasek@gmail.com>
2018-03-14 14:26:02 +0100

    end of 10th section

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,17 +1,37 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipesService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is test description', 'https://c1.staticflickr.com/5/4237/35094182543_f52a0ec1c5_b.jpg'),
-    new Recipe('A test recipe', 'this is test descrfgshsfhsfghiption', 'https://c1.staticflickr.com/5/4237/35094182543_f52a0ec1c5_b.jpg')
+    new Recipe('A test recipe',
+      'this is test description',
+      'https://c1.staticflickr.com/5/4237/35094182543_f52a0ec1c5_b.jpg',
+      [
+        new Ingredient('mushrooms', 5),
+        new Ingredient('zuccini', 2),
+        ]),
+    new Recipe('A test recipe',
+      'this is test descrfgshsfhsfghiption',
+      'https://c1.staticflickr.com/5/4237/35094182543_f52a0ec1c5_b.jpg',
+      [
+        new Ingredient('eggs', 2),
+        new Ingredient('pasta', 1)
+      ])
   ];
 
   getRecipes() {
     return this.recipes.slice();
   }
-  constructor() { }
 
+  constructor(private slService: ShoppingListService) {
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+
+
+  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,18 +3,30 @@ import {Ingredient} from '../shared/ingredient.model';
 
 @Injectable()
 export class ShoppingListService {
-  ingredients = new EventEmitter<Ingredient>();
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('apples', 5),
     new Ingredient('tomatoes', 10),
   ];
-getIngredients() {
-  return this.ingredients.slice();
-}
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
   constructor() {
   }
 
   addIngredient(ingredient: Ingredient) {
-  this.ingredients.push(ingredient);
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
+
+  addIngredients(ingredients: Ingredient[]) {
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+
 }
